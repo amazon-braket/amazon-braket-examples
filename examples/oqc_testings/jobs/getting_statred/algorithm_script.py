09ebb7c4fbd16bed1672c4c63654d41e9dc02961
Tim <91688157+yitchen-tim@users.noreply.github.com>
2022-02-18 16:11:37 -0500

    update notebook and script to run in Gamma

--- examples/oqc_testings/jobs/getting_statred/algorithm_script.py
+++ examples/oqc_testings/jobs/getting_statred/algorithm_script.py
@@ -14,15 +14,31 @@
 import os
 
 import numpy as np
-from braket.aws import AwsDevice
+from braket.aws import AwsDevice, AwsQuantumTask
 from braket.circuits import Circuit
 from braket.jobs import save_job_result
 
 
+
 print("Test job started!!!!!")
 
+
+# work around for Gamma
+import boto3
+from braket.aws import AwsQuantumTask, AwsSession
+region_name = "eu-west-2"
+endpoint_url = "https://braket-gamma.eu-west-2.amazonaws.com"
+braket_client = boto3.client("braket", region_name=region_name, endpoint_url=endpoint_url)
+aws_session = AwsSession(braket_client=braket_client)
+
+print("AWS session set!!!!!")
+
+
+
 # Use the device declared in the creation script
-device = AwsDevice(os.environ["AMZN_BRAKET_DEVICE_ARN"])
+device_arn = os.environ["AMZN_BRAKET_DEVICE_ARN"]
+device = AwsDevice(device_arn, aws_session=aws_session)
+# device = AwsDevice(device_arn)
 
 counts_list = []
 angle_list = []
@@ -32,6 +48,8 @@ for _ in range(5):
     random_circuit = Circuit().rx(0, angle)
 
     task = device.run(random_circuit, shots=100)
+#     task = AwsQuantumTask.create(aws_session, device_arn, random_circuit, s3_folder, shots=500)
+
     counts = task.result().measurement_counts
 
     angle_list.append(angle)
