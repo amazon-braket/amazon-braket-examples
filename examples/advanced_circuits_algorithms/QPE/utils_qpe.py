52eec28d80bf1aa85d820f9a4a3f9e9d215b1b97
Aaron Berdy <berdy@amazon.com>
2022-02-18 13:27:57 -0800

    feat: remove s3 folder configuration and upgrade aspen references to m1 (#110)
    Co-authored-by: Viraj Chaudhari <chauvira@amazon.com>

--- examples/advanced_circuits_algorithms/QPE/utils_qpe.py
+++ examples/advanced_circuits_algorithms/QPE/utils_qpe.py
@@ -199,7 +199,6 @@ def run_qpe(
     query_qubits,
     query_circuit,
     device,
-    s3_folder,
     items_to_keep=1,
     shots=1000,
     save_to_pck=False,
@@ -238,11 +237,7 @@ def run_qpe(
 
     # Run the circuit with all zeros input.
     # The query_circuit subcircuit generates the desired input from all zeros.
-    # The code below executes the correct device.run call depending on whether the backend is local or cloud based
-    if device.name in ("StateVectorSimulator", "DensityMatrixSimulator"):
-        task = device.run(circ, shots=shots)
-    else:
-        task = device.run(circ, s3_folder, shots=shots)
+    task = device.run(circ, shots=shots)
 
     # get result for this task
     result = task.result()
