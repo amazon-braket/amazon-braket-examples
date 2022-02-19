8be86a4e3fc953811580e0b0649289efcc69b240
Tim <91688157+yitchen-tim@users.noreply.github.com>
2022-02-18 22:36:44 -0500

    install staging repo via algorithm script

--- examples/oqc_testings/jobs/getting_started/script/algorithm_script.py
+++ examples/oqc_testings/jobs/getting_started/script/algorithm_script.py
@@ -1,22 +1,10 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"). You
-# may not use this file except in compliance with the License. A copy of
-# the License is located at
-#
-#     http://aws.amazon.com/apache2.0/
-#
-# or in the "license" file accompanying this file. This file is
-# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
-# ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the License.
-
+# first install packages (work around for pre-launch)
 import os
 import subprocess
 
-os.chdir("/opt/ml/code/customer_code/extracted/script/amazon-braket-schemas-python-staging")
+os.chdir("/opt/ml/code/customer_code/extracted/script/amazon-braket-schemas-python-staging-main")
 subprocess.run(["pip", "install", "."])
-os.chdir("/opt/ml/code/customer_code/extracted/script/amazon-braket-sdk-python-staging")
+os.chdir("/opt/ml/code/customer_code/extracted/script/amazon-braket-sdk-python-staging-main")
 subprocess.run(["pip", "install", "."])
 os.chdir("/")
 
