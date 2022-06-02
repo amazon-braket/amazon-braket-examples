f57948d405e5439a8aa2fad2044ac54e1b269c61
Lin <licedric@amazon.com>
2022-06-01 23:46:27 -0700

    Update environment & test for photonics

--- test/notebook_tests/test_notebooks.py
+++ test/notebook_tests/test_notebooks.py
@@ -27,6 +27,7 @@ test_notebooks = []
 
 # These notebooks would not be tested.
 EXCLUDED_NOTEBOOKS = [
+    "4_Operating_Borealis_beginner_tutorial.ipynb",
     "bring_your_own_container.ipynb"
 ]
 
