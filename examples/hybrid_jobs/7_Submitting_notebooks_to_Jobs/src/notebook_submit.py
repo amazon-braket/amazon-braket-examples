45a0fa1070ffdd85012fca81c9227391c016243e
Matt Beach <drbeach@amazon.com>
2022-11-04 11:32:15 -0400

    fix: steps and formatting

---
+++ examples/hybrid_jobs/7_Submitting_notebooks_to_Jobs/src/notebook_submit.py
@@ -0,0 +1,35 @@
+import os
+import tarfile
+
+from boto3.session import Session
+from braket.aws import AwsQuantumJob
+
+
+def submit_notebook(notebook, device_arn, hyperparameters):
+    job = AwsQuantumJob.create(
+        device=device_arn,
+        source_module="src",
+        entry_point="src.runner",
+        hyperparameters=hyperparameters,
+        input_data=notebook,
+    )
+    return job
+
+
+def download_result_notebook(job, dir_name="result"):
+    job.result()
+    PATH = f"{dir_name}"
+    if not os.path.exists(PATH):
+        os.makedirs(PATH)
+
+    path = job.metadata()["outputDataConfig"]["s3Path"] + "/output/model.tar.gz"
+    bucket_name = path.split("/")[2]
+    output_path = "/".join(path.split("/")[3:])
+
+    session = Session()
+    s3 = session.resource("s3")
+    your_bucket = s3.Bucket(bucket_name)
+    your_bucket.download_file(output_path, f"{dir_name}/model.tar.gz")
+
+    with tarfile.open(f"{dir_name}/model.tar.gz") as file:
+        file.extractall(f"./{dir_name}")
--- examples/hybrid_jobs/7_Submitting_notebooks_to_Jobs/src/runner.py
+++ examples/hybrid_jobs/7_Submitting_notebooks_to_Jobs/src/runner.py
@@ -13,45 +13,43 @@ def convert_to_value(value):
         return value
 
 
-def load_hyperparams():
-    # Load Braket Jobs hyperparameters
+def load_jobs_hyperparams():
     with open(os.environ["AMZN_BRAKET_HP_FILE"]) as f:
         braket_hyperparams = json.load(f)
-
     print(f"Braket hyperparameters are: {braket_hyperparams}")
+    return braket_hyperparams
 
-    # Convert Braket hyperparameters to Papermill parameters
+
+def convert_jobs_hyperparams_to_pm_params(braket_hyperparams):
     papermill_params = {}
     for key, value in braket_hyperparams.items():
         papermill_params[key] = convert_to_value(value)
-
     papermill_params["device_arn"] = os.environ["AMZN_BRAKET_DEVICE_ARN"]
     papermill_params["results_dir"] = os.environ["AMZN_BRAKET_JOB_RESULTS_DIR"]
-
     print(f"Papermill parameters are: {papermill_params}")
     return papermill_params
 
 
-def run_notebook():
-    papermill_params = load_hyperparams()
+def get_notebook_name(input_dir):
+    notebooks = list(glob.glob(f"{input_dir}/input/*.ipynb"))
+    if len(notebooks) > 1:
+        raise ValueError("To many input notebooks provided.")
+    notebook_name = notebooks[0].split("/")[-1]
+    return notebook_name
 
+
+def run_notebook():
     results_dir = os.environ.get("AMZN_BRAKET_JOB_RESULTS_DIR")
     input_dir = os.environ["AMZN_BRAKET_INPUT_DIR"]
 
-    notebooks = list(glob.glob(f"{input_dir}/input/*.ipynb"))
-    print(f"Notebooks are {notebooks}")
+    braket_hyperparams = load_jobs_hyperparams()
 
-    if len(notebooks) > 1:
-        raise ValueError("To many input notebooks provided.")
+    papermill_params = convert_jobs_hyperparams_to_pm_params(braket_hyperparams)
 
-    notebook_name = notebooks[0].split("/")[-1]
-    filename = f"{input_dir}/input/{notebook_name}"
-    print("Loading dataset from: ", filename)
-
-    print(f"Saving results to {results_dir}/{notebook_name}")
+    notebook_name = get_notebook_name(input_dir)
 
     pm.execute_notebook(
-        filename,
+        f"{input_dir}/input/{notebook_name}",
         f"{results_dir}/{notebook_name}",
         parameters=papermill_params,
         kernel_name="python3",
