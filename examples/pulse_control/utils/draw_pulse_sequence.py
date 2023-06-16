fddc18d1c4b589f96f0dc616ca716f9372f6f50d
Jean-Christophe Jaskula <jjaskula@amazon.com>
2023-06-16 17:40:10 -0400

    Add sweep with jobs and native gate calibrations

---
+++ examples/pulse_control/utils/draw_pulse_sequence.py
@@ -0,0 +1,49 @@
+from braket.pulse import PulseSequence
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def draw(pulse_sequence: PulseSequence):
+    data = pulse_sequence.to_time_trace()
+    for frame_id in data.amplitudes:
+        f, ax = plt.subplots(nrows=3, sharex=True)
+        f.subplots_adjust(hspace=0)
+
+        ax[0].set_title(frame_id)
+        ax[0].plot(data.amplitudes[frame_id].times(), np.real(data.amplitudes[frame_id].values()), label="Real")
+        ax[0].plot(data.amplitudes[frame_id].times(), np.imag(data.amplitudes[frame_id].values()), label="Imag")
+        ax[0].set_ylabel("Amplitude\n(a. u.)")
+        ax[0].tick_params('x', labelbottom=False)
+
+        ax[1].plot(data.frequencies[frame_id].times(), np.array(data.frequencies[frame_id].values())*1e-9)
+        ax[1].set_ylabel("Frequency\n(GHz)")
+        ax[1].tick_params('x', labelbottom=False)
+
+        ax[2].plot(data.phases[frame_id].times(), data.phases[frame_id].values())
+        ax[2].set_xlabel("Time (s)")
+        ax[2].set_ylabel("Phase\n(rad)")
+
+
+def draw_multiple_frames(pulse_sequence: PulseSequence):
+    data = pulse_sequence.to_time_trace()
+    for frame_id in data.amplitudes:
+        f=plt.figure(figsize=(12,4))
+        plt.subplot(1,3,1)
+        plt.plot(data.amplitudes[frame_id].times(), np.real(data.amplitudes[frame_id].values()), label="Real")
+        plt.plot(data.amplitudes[frame_id].times(), np.imag(data.amplitudes[frame_id].values()), label="Imag")
+        plt.xlabel("Time (s)")
+        plt.ylabel("Amplitude (norm.)")
+        plt.legend(loc="upper right")
+
+        plt.subplot(1,3,2)
+        plt.title(frame_id)
+        plt.plot(data.frequencies[frame_id].times(), data.frequencies[frame_id].values())
+        plt.xlabel("Time (s)")
+        plt.ylabel("Frequency (Hz)")
+
+        plt.subplot(1,3,3)
+        plt.plot(data.phases[frame_id].times(), data.phases[frame_id].values())
+        plt.xlabel("Time (s)")
+        plt.ylabel("Phase (rad)")
+        f.tight_layout()
