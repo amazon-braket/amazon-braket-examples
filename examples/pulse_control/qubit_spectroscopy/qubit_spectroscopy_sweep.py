9caca881a1e38a1f3d95595d3ada2440768280dd
Jean-Christophe Jaskula <jjaskula@amazon.com>
2023-07-11 12:26:15 -0400

    fresh executed notebooks

--- examples/pulse_control/qubit_spectroscopy/qubit_spectroscopy_sweep.py
+++ examples/pulse_control/qubit_spectroscopy/qubit_spectroscopy_sweep.py
@@ -1,4 +1,4 @@
-#! Braket jobs 
+#! Braket jobs
 
 import json
 import os
@@ -19,7 +19,7 @@ print("Test job started!")
 device_arn = os.environ["AMZN_BRAKET_DEVICE_ARN"]
 device = AwsDevice(device_arn)
 
-#Load the Hybrid Job hyperparameters
+# Load the Hybrid Job hyperparameters
 hp_file = os.environ["AMZN_BRAKET_HP_FILE"]
 with open(hp_file, "r") as f:
     hyperparams = json.load(f)
@@ -36,7 +36,7 @@ experiment_configuration = {
         "drive_frame": f"q{qubit}_drive",
         "readout_frame": f"r{qubit}_measure",
         "spectroscopy_wf": ConstantWaveform(25e-9, 0.03),
-    }
+    },
 }
 drive_frame = device.frames[experiment_configuration[device_arn]["drive_frame"]]
 readout_frame = device.frames[experiment_configuration[device_arn]["readout_frame"]]
@@ -44,36 +44,52 @@ waveform = GaussianWaveform(100e-9, 25e-9, 0.1, True)
 
 N_steps = int(hyperparams.get("N_steps", 25))
 N_shots = int(hyperparams.get("N_shots", 100))
-span = 75e6 if not ("frequency_start" in hyperparams and "frequency_stop" in hyperparams) else None
-frequency_start =  float(hyperparams.get("frequency_start", drive_frame.frequency-span/2))
-frequency_stop =  float(hyperparams.get("frequency_stop", drive_frame.frequency+span/2))
+span = (
+    75e6
+    if not ("frequency_start" in hyperparams and "frequency_stop" in hyperparams)
+    else None
+)
+frequency_start = float(
+    hyperparams.get("frequency_start", drive_frame.frequency - span / 2)
+)
+frequency_stop = float(
+    hyperparams.get("frequency_stop", drive_frame.frequency + span / 2)
+)
 
 frequency_free_parameter = FreeParameter("frequency")
-pulse_sequence = ( 
-        PulseSequence()
-        .set_frequency(drive_frame, frequency_free_parameter)
-        .play(drive_frame, waveform)
-        .capture_v0(readout_frame)
-    )
+pulse_sequence = (
+    PulseSequence()
+    .set_frequency(drive_frame, frequency_free_parameter)
+    .play(drive_frame, waveform)
+    .capture_v0(readout_frame)
+)
 
 frequencies = np.linspace(frequency_start, frequency_stop, N_steps)
 populations = []
 
 for i, frequency in enumerate(frequencies):
-    task = device.run(pulse_sequence(frequency=frequency), shots=N_shots, tags={"frequency": str(frequency)})
+    task = device.run(
+        pulse_sequence(frequency=frequency),
+        shots=N_shots,
+        tags={"frequency": str(frequency)},
+    )
     counts = task.result().measurement_counts
 
-    population_zero = counts['0']/N_shots
+    population_zero = counts["0"] / N_shots
     populations.append(population_zero)
 
-    braket_tasks_cost = float(cost_tracker.simulator_tasks_cost() + cost_tracker.qpu_tasks_cost())
-    log_metric(metric_name="braket_tasks_cost", value=braket_tasks_cost, iteration_number=i)
+    braket_tasks_cost = float(
+        cost_tracker.simulator_tasks_cost() + cost_tracker.qpu_tasks_cost()
+    )
+    log_metric(
+        metric_name="braket_tasks_cost", value=braket_tasks_cost, iteration_number=i
+    )
 
 # Save the variables of interest so that we can access later
 save_job_result(
     {
         "populations in |0>": populations,
-        "frequencies": frequencies.tolist(), 
+        "frequencies": frequencies.tolist(),
         "task summary": cost_tracker.quantum_tasks_statistics(),
         "estimated cost": braket_tasks_cost,
     }
