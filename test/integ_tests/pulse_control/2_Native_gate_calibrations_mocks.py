54948631cd03826ffb356dfaace91a0566d9aa15
Abe Coull <85974725+math411@users.noreply.github.com>
2023-08-15 23:09:28 +0000

    update results and add provider name to mocks

--- test/integ_tests/pulse_control/2_Native_gate_calibrations_mocks.py
+++ test/integ_tests/pulse_control/2_Native_gate_calibrations_mocks.py
@@ -5,6 +5,7 @@ def pre_run_inject(mock_utils):
     mock_utils.mock_default_device_calls(mocker)
     mocker.set_get_device_result({
         "deviceType" : "QPU",
+        "providerName": "Rigetti",
         "deviceCapabilities" : mock_utils.read_file("oqc_pulse_device_capabilities.json", __file__)
     })
     mocker.set_task_result_return(mock_utils.read_file("pulse_results.json", __file__))
