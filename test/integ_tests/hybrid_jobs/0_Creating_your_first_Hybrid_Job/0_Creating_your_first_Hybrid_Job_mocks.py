e6398125e9915ce514d436abe4b81f6a78984077
AjinkyaGawali <79669399+AjinkyaGawali@users.noreply.github.com>
2025-03-03 15:43:04 -0800

    fix: Modified integ tests (#688)

--- test/integ_tests/hybrid_jobs/0_Creating_your_first_Hybrid_Job/0_Creating_your_first_Hybrid_Job_mocks.py
+++ test/integ_tests/hybrid_jobs/0_Creating_your_first_Hybrid_Job/0_Creating_your_first_Hybrid_Job_mocks.py
@@ -10,8 +10,8 @@ def pre_run_inject(mock_utils):
     mocker = mock_utils.Mocker()
     mock_utils.mock_default_device_calls(mocker)
     mock_utils.mock_default_job_calls(mocker)
-    mocker.set_create_job_side_effect(
-            lambda **kwargs: {"jobArn": f"arn:aws:braket:{mocker.region_name}:000000:job/testJob"}
+    mocker.set_create_job_result(
+        {"jobArn": f"arn:aws:braket:{mocker.region_name}:000000:job/testJob"}
     )
     mocker.set_log_streams_result({"logStreams": []})
     mocker.set_get_query_results_result(
