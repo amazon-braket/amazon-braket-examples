18c131cfdd645baa18b1d7b121e6d07e6c27a335
Cody Wang <speller26@gmail.com>
2025-08-14 15:17:15 -0700

    Update 4_Dynamic_Circuits_with_Qiskit_Braket_Provider_mocks.py

--- test/integ_tests/experimental_capabilities/dynamic_circuits/4_Dynamic_Circuits_with_Qiskit_Braket_Provider_mocks.py
+++ test/integ_tests/experimental_capabilities/dynamic_circuits/4_Dynamic_Circuits_with_Qiskit_Braket_Provider_mocks.py
@@ -1,6 +1,9 @@
 def pre_run_inject(mock_utils):
     mocker = mock_utils.Mocker()
     mock_utils.mock_default_device_calls(mocker)
+    mocker._wrapper.boto_client.get_device.return_value[
+        "deviceCapabilities"
+    ]["action"]["braket.ir.openqasm.program"]["supportedOperations"].remove("kraus")
     effects = [mock_utils.read_file("results/results_4_0.json", __file__)]
     mocker.set_task_result_side_effect(effects)
 
