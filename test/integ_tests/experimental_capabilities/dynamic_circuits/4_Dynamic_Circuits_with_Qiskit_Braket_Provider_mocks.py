2379029d5bfabec8a2902b2f06a53f44c4d9229a
Cody Wang <speller26@gmail.com>
2025-08-14 15:30:50 -0700

    Update 4_Dynamic_Circuits_with_Qiskit_Braket_Provider_mocks.py

--- test/integ_tests/experimental_capabilities/dynamic_circuits/4_Dynamic_Circuits_with_Qiskit_Braket_Provider_mocks.py
+++ test/integ_tests/experimental_capabilities/dynamic_circuits/4_Dynamic_Circuits_with_Qiskit_Braket_Provider_mocks.py
@@ -1,9 +1,16 @@
+import json
+
+
 def pre_run_inject(mock_utils):
     mocker = mock_utils.Mocker()
     mock_utils.mock_default_device_calls(mocker)
-    mocker._wrapper.boto_client.get_device.return_value[
-        "deviceCapabilities"
-    ]["action"]["braket.ir.openqasm.program"]["supportedOperations"].remove("kraus")
+    capabilities = json.loads(
+        mocker._wrapper.boto_client.get_device.return_value["deviceCapabilities"]
+    )
+    capabilities["action"]["braket.ir.openqasm.program"]["supportedOperations"].remove("kraus")
+    mocker._wrapper.boto_client.get_device.return_value["deviceCapabilities"] = json.dumps(
+        capabilities
+    )
     effects = [mock_utils.read_file("results/results_4_0.json", __file__)]
     mocker.set_task_result_side_effect(effects)
 
