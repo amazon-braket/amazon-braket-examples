25625b866a00352bc92804029a2c781c83bbc423
Tim (Yi-Ting) <yitchen@amazon.com>
2025-08-15 19:15:02 -0400

    support mocking program set results (#737)

--- test/integ_tests/experimental_capabilities/dynamic_circuits/4_Dynamic_Circuits_with_Qiskit_Braket_Provider_mocks.py
+++ test/integ_tests/experimental_capabilities/dynamic_circuits/4_Dynamic_Circuits_with_Qiskit_Braket_Provider_mocks.py
@@ -1,17 +1,6 @@
 def pre_run_inject(mock_utils):
     mocker = mock_utils.Mocker()
-    mock_utils.mock_default_device_calls(mocker)
-    mocker.set_get_device_result(
-        {
-            "deviceType": "QPU",
-            "deviceCapabilities": mock_utils.read_file(
-                "garnet_device_capabilities_without_programset.json",
-                __file__,
-            ),
-        },
-    )
-    effects = [mock_utils.read_file("results/results_4_0.json", __file__)]
-    mocker.set_task_result_side_effect(effects)
+    mock_utils.mock_default_device_program_set_calls(mocker)
 
 
 def post_run(tb):
