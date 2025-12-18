0a45dd3ce2c4275e19fa0a9101884c8e91515f4d
Cody Wang <speller26@gmail.com>
2025-12-17 17:05:15 -0800

    Update 3_Deep_dive_into_the_anatomy_of_quantum_circuits_mocks.py

--- test/integ_tests/getting_started/3_Deep_dive_into_the_anatomy_of_quantum_circuits/3_Deep_dive_into_the_anatomy_of_quantum_circuits_mocks.py
+++ test/integ_tests/getting_started/3_Deep_dive_into_the_anatomy_of_quantum_circuits/3_Deep_dive_into_the_anatomy_of_quantum_circuits_mocks.py
@@ -1,40 +1,31 @@
 import os
+from itertools import cycle
+from unittest import mock
 
 
 def pre_run_inject(mock_utils):
     mocker = mock_utils.Mocker()
     mock_utils.mock_default_device_calls(mocker)
     mocker.set_get_quantum_task_side_effect(
-        [
-            {
-                "quantumTaskArn": "arn:aws:braket:us-west-2:000000:quantum-task/TestARN",
-                "status": "RUNNING",
-                "outputS3Bucket": "Test Bucket",
-                "outputS3Directory": "Test Directory",
-                "shots": 10,
-                "deviceArn": "Test Device Arn",
-                "queueInfo": {
-                    "queue": "QUANTUM_TASKS_QUEUE",
-                    "position": "2",
-                    "queuePriority": "Normal",
+        cycle(
+            [
+                {
+                    "quantumTaskArn": "arn:aws:braket:us-west-2:000000:quantum-task/TestARN",
+                    "status": "RUNNING",
+                    "outputS3Bucket": "Test Bucket",
+                    "outputS3Directory": "Test Directory",
+                    "shots": 10,
+                    "deviceArn": "Test Device Arn",
+                    "queueInfo": {
+                        "queue": "QUANTUM_TASKS_QUEUE",
+                        "position": "2",
+                        "queuePriority": "Normal",
+                    },
+                    "ResponseMetadata": {"HTTPHeaders": {"date": ""}},
                 },
-                "ResponseMetadata": {"HTTPHeaders": {"date": ""}},
-            },
-            {
-                "quantumTaskArn": "arn:aws:braket:us-west-2:000000:quantum-task/TestARN",
-                "status": "COMPLETED",
-                "outputS3Bucket": "Test Bucket",
-                "outputS3Directory": "Test Directory",
-                "shots": 10,
-                "deviceArn": "Test Device Arn",
-                "queueInfo": {
-                    "queue": "QUANTUM_TASKS_QUEUE",
-                    "position": "2",
-                    "queuePriority": "Normal",
-                },
-                "ResponseMetadata": {"HTTPHeaders": {"date": ""}},
-            },
-        ]
+                mock.DEFAULT,
+            ]
+        )
     )
     mocker.set_cancel_quantum_task_result(
         {
