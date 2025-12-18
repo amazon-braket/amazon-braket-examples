e9f8082d3b3ca624014a708b7a155bbe0b649bac
Cody Wang <speller26@gmail.com>
2025-12-17 16:54:19 -0800

    Correct fixture

--- test/integ_tests/getting_started/3_Deep_dive_into_the_anatomy_of_quantum_circuits/3_Deep_dive_into_the_anatomy_of_quantum_circuits_mocks.py
+++ test/integ_tests/getting_started/3_Deep_dive_into_the_anatomy_of_quantum_circuits/3_Deep_dive_into_the_anatomy_of_quantum_circuits_mocks.py
@@ -4,6 +4,38 @@ import os
 def pre_run_inject(mock_utils):
     mocker = mock_utils.Mocker()
     mock_utils.mock_default_device_calls(mocker)
+    mocker.set_get_quantum_task_side_effect(
+        [
+            {
+                "quantumTaskArn": "arn:aws:braket:us-west-2:000000:quantum-task/TestARN",
+                "status": "RUNNING",
+                "outputS3Bucket": "Test Bucket",
+                "outputS3Directory": "Test Directory",
+                "shots": 10,
+                "deviceArn": "Test Device Arn",
+                "queueInfo": {
+                    "queue": "QUANTUM_TASKS_QUEUE",
+                    "position": "2",
+                    "queuePriority": "Normal",
+                },
+                "ResponseMetadata": {"HTTPHeaders": {"date": ""}},
+            },
+            {
+                "quantumTaskArn": "arn:aws:braket:us-west-2:000000:quantum-task/TestARN",
+                "status": "COMPLETED",
+                "outputS3Bucket": "Test Bucket",
+                "outputS3Directory": "Test Directory",
+                "shots": 10,
+                "deviceArn": "Test Device Arn",
+                "queueInfo": {
+                    "queue": "QUANTUM_TASKS_QUEUE",
+                    "position": "2",
+                    "queuePriority": "Normal",
+                },
+                "ResponseMetadata": {"HTTPHeaders": {"date": ""}},
+            },
+        ]
+    )
     mocker.set_cancel_quantum_task_result(
         {
             "cancellationStatus": "CANCELLING",
