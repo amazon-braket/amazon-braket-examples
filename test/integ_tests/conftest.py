775550133468b7abe32361470fc6edbd43930a07
Scott Smart <42444435+sesmart@users.noreply.github.com>
2026-02-19 16:26:31 -0500

    add conftest.py

--- test/integ_tests/conftest.py
+++ test/integ_tests/conftest.py
@@ -8,7 +8,22 @@ def pytest_addoption(parser):
         default="ALL",
         help="ALL=mock everything, LEAST=mock least possible",
     )
+    parser.addoption(
+        "--test-mitiq",
+        action="store_true",
+        default=False,
+        help="specify to include execute tests requiring mitiq",
+    )
 
+def pytest_configure(config):
+    config.addinivalue_line("markers", "mitiq: tests only specific to mitiq-related notebooks")
+
+def pytest_collection_modifyitems(config, items):
+    if not config.getoption("--test-mitiq"):
+        skip_mitiq = pytest.mark.skip(reason="need --test-mitiq option to run")
+        for item in items:
+            if "mitiq" in item.keywords:
+                item.add_marker(skip_mitiq)
 
 @pytest.fixture
 def mock_level(request):
