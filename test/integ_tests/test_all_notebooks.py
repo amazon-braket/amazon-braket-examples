638e30e32ec5e810e76e843af46b1470b09dce84
Scott Smart <42444435+sesmart@users.noreply.github.com>
2026-02-20 13:42:53 -0500

    tests: add test-mitiq flag to pytest and default to false (#843)
    Co-authored-by: Tim (Yi-Ting) <yitchen@amazon.com>

--- test/integ_tests/test_all_notebooks.py
+++ test/integ_tests/test_all_notebooks.py
@@ -5,7 +5,6 @@ from importlib.machinery import SourceFileLoader
 
 import pytest
 from jupyter_client import kernelspec
-from nbconvert import HTMLExporter
 from testbook import testbook
 
 UNCOMMENT_NOTEBOOK_TAG = "## UNCOMMENT_TO_RUN"
@@ -76,10 +75,6 @@ if (
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-if "integ_tests" in os.getcwd():
-    os.chdir(os.path.join("..", ".."))
-
-root_path = os.getcwd()
 examples_path = "examples"
 test_notebooks = []
 
@@ -106,23 +101,12 @@ def get_mock_paths(notebook_dir, notebook_file):
     path_to_utils = os.path.join(path_to_root, "test", "integ_tests", "mock_utils.py")
     return path_to_utils, path_to_mocks
 
-@pytest.fixture(scope="module")
-def html_exporter():
-    return HTMLExporter(template_name="classic")
-
-@pytest.fixture(autouse=True)
-def restore_cwd():
-    """ after each test, move back to root_path - amazon-braket-examples/"""
-    yield
-    os.chdir(root_path)
-
-
 @pytest.mark.parametrize("notebook_dir, notebook_file", test_notebooks)
 def test_all_notebooks(notebook_dir, notebook_file, mock_level):
     if notebook_file in EXCLUDED_NOTEBOOKS:
         pytest.skip(f"Skipping Notebook: '{notebook_file}'")
-
     os.chdir(notebook_dir)
+
     path_to_utils, path_to_mocks = get_mock_paths(notebook_dir, notebook_file)
     # Try to use the conda_braket kernel if installed, otherwise fall back to the default value of python3
     kernel = "conda_braket" if "conda_braket" in kernelspec.find_kernel_specs() else "python3"
@@ -250,10 +234,10 @@ def uncomment_test_section(source):
 
 
 def test_not_imported():
-    """ verify that certain libaries have not been imported 
+    """ This verifies that certain tests were not imported - do not remove!
     
     Excluded libraries: 
-        mitiq - see `/examples/error_mitigation/on_mitiq/NOTICE.md` for details
+        mitiq - see `/examples/error_mitigation/on_mitiq/README.md` for details
     """
     extra_libraries = ["mitiq"]
     import importlib
