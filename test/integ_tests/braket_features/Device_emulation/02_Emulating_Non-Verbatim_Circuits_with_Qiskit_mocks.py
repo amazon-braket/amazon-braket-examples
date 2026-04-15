a7ce0a94feb40abd5144255bce30725b1cd123b9
maolinml <86260930+maolinml@users.noreply.github.com>
2026-04-15 11:22:08 -0700

    Add integ test mock for Qiskit emulation notebook

---
+++ test/integ_tests/braket_features/Device_emulation/02_Emulating_Non-Verbatim_Circuits_with_Qiskit_mocks.py
@@ -0,0 +1,36 @@
+def pre_run_inject(mock_utils):
+    mocker = mock_utils.Mocker()
+    mock_utils.mock_default_device_calls(mocker)
+    mocker.set_get_device_result(
+        {
+            "deviceType": "QPU",
+            "providerName": "Rigetti",
+            "deviceCapabilities": mock_utils.read_file("ankaa3_device_capabilities.json"),
+            "deviceQueueInfo": [
+                {"queue": "QUANTUM_TASKS_QUEUE", "queueSize": "0", "queuePriority": "Normal"},
+                {"queue": "QUANTUM_TASKS_QUEUE", "queueSize": "0", "queuePriority": "Priority"},
+                {"queue": "JOBS_QUEUE", "queueSize": "0"},
+            ],
+        },
+    )
+    mocker.set_task_result_return(mock_utils.read_file("default_results.json"))
+    mocker.set_search_result(
+        [
+            {
+                "devices": [
+                    {
+                        "deviceArn": "arn:aws:braket:us-west-1::device/qpu/rigetti/Ankaa-3",
+                        "deviceName": "Ankaa-3",
+                        "deviceType": "QPU",
+                        "deviceStatus": "ONLINE",
+                        "providerName": "Rigetti",
+                    },
+                ],
+            },
+        ],
+    )
+
+
+def post_run(tb):
+    pass
+
